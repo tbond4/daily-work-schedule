75795f0acb46ca2b8ebacb7b5c8dfd201a4fcf4d
Tristan Bond <tristanebond@gmail.com>
2020-11-06 20:55:57 -0800

    js comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var timeEl=$("#currentDay");
-var hour= moment().format("h");
+var hour= 0;
 var nineBlock =$("#9Block");
 var tenBlock=$("#10Block");
 var elevenBlock=$("#11Block");
@@ -12,35 +12,36 @@ var fiveBlock=$("#5Block");
 var blockArr=[nineBlock,tenBlock,elevenBlock,twelveBlock,oneBlock,twoBlock,threeBlock,fourBlock,fiveBlock]
 var container=$(".container");
 
-
+// Update time element and call dispaly color
 setInterval(function(){
     timeEl.text(moment().format("dddd, MMMM Do h:m" ))
     hour=moment().format("h"); 
     displayColors();
 },1000)
-
+//color the current hour block red and future blocks green
 function displayColors(){
     for(var i = 0;i<blockArr.length;i++){
     if(blockArr[i].data("time") == hour){
        var timeBlock = blockArr[i];
         blockArr[i].css("background-color", "red");
+        blockArr[i].find("input").css("background-color", "red");
         for(var j = 0;j<blockArr.length;j++){
-            console.log(blockArr[j].data("block"));
-            console.log(timeBlock.data("block"));
             if(blockArr[j].data("block")>timeBlock.data("block")){
                 blockArr[j].css("background-color", "green");
+                blockArr[j].find("input").css("background-color", "green");
             }
         }
     }
 }
 }
+//event listener that delegates to buttons, saves input to local storage
 container.on( "click", "button", function(event){
    var task= $(event.target).parent().parent().find("input").val();
    var blockNum=$(event.target).parent().parent().data("block");
    localStorage.setItem(blockNum, task);
 })
 
-nineBlock.find("input").attr("placeholder", localStorage.getItem(nineBlock.data("block")));
+//pulls from local storage
 for(var i=0;i<blockArr.length;i++){
     blockArr[i].find("input").attr("placeholder", localStorage.getItem(blockArr[i].data("block")));
 }
