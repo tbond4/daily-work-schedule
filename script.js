8189283018df61022a9a48da3ee6b1ae9d4e746c
Tristan Bond <tristanebond@gmail.com>
2020-11-06 18:40:10 -0800

    time block color designations

--- script.js
+++ script.js
@@ -10,18 +10,27 @@ var threeBlock=$("#3Block");
 var fourBlock=$("#4Block");
 var fiveBlock=$("#5Block");
 var blockArr=[nineBlock,tenBlock,elevenBlock,twelveBlock,oneBlock,twoBlock,threeBlock,fourBlock,fiveBlock]
+var container=$(".container");
+
+
 setInterval(function(){
     timeEl.text(moment().format("dddd, MMMM Do h:m" ))
-    hour=moment().format("h");
-    
-})
-for(var i = 0;i<blockArr.length;i++){
-    
-    console.log(blockArr[i].data("time"));
-    console.log(hour);
+    hour=moment().format("h"); 
+    displayColors();
+},1000)
+
+function displayColors(){
+    for(var i = 0;i<blockArr.length;i++){
     if(blockArr[i].data("time") == hour){
-       
+       var timeBlock = blockArr[i];
         blockArr[i].css("background-color", "red");
+        for(var j = 0;j<blockArr.length;j++){
+            console.log(blockArr[j].data("block"));
+            console.log(timeBlock.data("block"));
+            if(blockArr[j].data("block")>timeBlock.data("block")){
+                blockArr[j].css("background-color", "green");
+            }
+        }
     }
 }
-
+}
